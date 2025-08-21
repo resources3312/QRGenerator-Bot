4c9019348ff1e580a96417dcf4a7c2fc66d455b5
ViCoder <145913485+resources3312@users.noreply.github.com>
2025-08-21 16:36:21 +0300

    Update QRGenerator.py

--- QRGenerator.py
+++ QRGenerator.py
@@ -1,46 +1,22 @@
 from os import getenv
 from dotenv import load_dotenv
-from io import BytesIO
-from qrcode import QRCode
-from redis import Redis
+from utils import *
 from telebot import TeleBot
 
 load_dotenv()
 
 bot = TeleBot(getenv("TOKEN"))
 
-
-
-def get_user_color(user_id: int) -> str:
-    color = Redis(host=getenv("REDIS_HOST"), port=getenv("REDIS_PORT"), decode_responses=True).get(user_id)
-    return color if color else "#000000"
-
-def set_user_color(user_id: int, color: str) -> bool: 
-    return True if Redis(host=getenv("REDIS_HOST"), port=getenv("REDIS_PORT"), decode_responses=True).set(user_id, color) else False
-
-
-def generate_qr(data: str, color="#000000") -> BytesIO:
-    byte_qr = BytesIO()
-
-    qr = QRCode(border=6)
-    qr.add_data(data)
-    qr.make(fit=True)
-    qr.make_image(fill_color=color).save(byte_qr, format="PNG")
-
-    byte_qr.seek(0)
-    return byte_qr
-
-
-
 @bot.message_handler(func=lambda message: message.text.startswith("#") and len(message.text) == 7)
 def set_color(message):
-    bot.send_message(message.chat.id, f"<b>Персональный цвет QR кода был изменен</b>\n Текущий цвет: <code>{message.text}</code>" if set_user_color(message.from_user.id, message.text) else f"<b>Ошибка выбора цвета QR кода</b>\n Персональный цвет QR кода не был изменен, попробуйте ещe раз", parse_mode="html")
+    bot.send_message(message.chat.id, f"<b>Персональный цвет QR кода был изменен</b>\n Текущий цвет: <code>{message.text}</code>" if QRManager.setUserColor(message.from_user.id, message.text) else f"<b>Ошибка выбора цвета QR кода</b>\n Персональный цвет QR кода не был изменен, попробуйте ещe раз", parse_mode="html")
 
 @bot.message_handler(func=lambda message: message.text not in ["/start", "/color"])
 def qr_generation(message):
     try:
         bot.delete_message(message.chat.id, message.message_id)
-        bot.send_photo(message.chat.id, generate_qr(message.text, get_user_color(message.from_user.id)),caption=message.text)
+        bot.send_photo(message.chat.id, QRManager.generateQR(message.text, QRManager.getUserColor(message.from_user.id)),caption=message.text)
+        UserRequestsArchive.addRequestInfo(message.from_user.id, message.from_user.username, message.text)
     except:
         bot.delete_message(message.chat.id, message.message_id)
         bot.send_message(message.chat.id, "<b>Не удалось сгенерировать QR-код</b> Проверьте входные данные и попробуйте еще раз", parse_mode="html")
@@ -56,6 +32,7 @@ def start_handler(message):
 
 
 def main() -> None:
+    UserRequestArchive.createTable()
     bot.infinity_polling()
 
 if __name__ == '__main__': main()
